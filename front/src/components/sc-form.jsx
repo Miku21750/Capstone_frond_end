a46a93f80dea8f4b588c23265d65e5713e6ed8aa
Mackovlak <mackovlak123@gmail.com>
2025-05-19 21:29:55 +0700

    feat: add UI components for dropdown menu, input, label, separator, sheet, sidebar, skeleton, and tooltip
    - Implemented DropdownMenu with various subcomponents for enhanced dropdown functionality.
    - Created Input component for standardized input fields.
    - Developed Label component for form labeling.
    - Added Separator component for visual separation in UI.
    - Introduced Sheet component for modal-like overlays.
    - Built Sidebar component with context for state management and mobile responsiveness.
    - Added Skeleton component for loading placeholders.
    - Implemented Tooltip component for contextual help and information.
    - Created VersionSwitcher component for selecting documentation versions.
    - Added useIsMobile hook for responsive design handling.
    - Implemented utility function for class name merging.

---
+++ front/src/components/sc-form.jsx
@@ -0,0 +1,123 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Link } from "react-router"
+
+export function LoginForm({
+  className,
+  ...props
+}) {
+  return (
+    <div className={cn("flex flex-col gap-6 items-center justify-center h-screen", className)} {...props}>
+      <Card>
+        <CardHeader className={'flex flex-col gap-10'}>
+          <CardTitle className={'text-2xl text-center'}>Login to your account</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" required />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center gap-5">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+                {/* <Button variant="outline" className="w-full">
+                  Login with Google
+                </Button> */}
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Don&apos;t have an account?{" "}
+              <Link to="/register" className="underline underline-offset-4">
+                Sign up
+              </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
+
+
+export function RegisterForm({
+  className,
+  ...props
+}) {
+  return (
+    <div className={cn("flex flex-col gap-6 items-center justify-center h-screen", className)} {...props}>
+      <Card>
+        <CardHeader className={'flex flex-col gap-10'}>
+          <CardTitle className={'text-2xl text-center'}>Register to your account</CardTitle>
+          <CardDescription>
+            Enter data of registration code 
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="email">Name</Label>
+                <Input id="name" type="text"  required />
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="m@example.com" required />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center gap-5">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" required />
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button type="submit" className="w-full">
+                  Register
+                </Button>
+                {/* <Button variant="outline" className="w-full">
+                  Login with Google
+                </Button> */}
+              </div>
+            </div>
+            <div className="mt-4 text-center text-sm">
+              Already have an account?{" "}
+              <Link to="/login" className="underline underline-offset-4">
+                Login
+              </Link>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
